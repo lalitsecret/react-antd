7b35c5c819a75ed04280d5882f925b011b0eac81
lalitsecret <lalitsecret@gmail.com>
2021-03-09 11:53:54 +0530

    1 file modified

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import Home from './Home'
+import Head from './Head'
+import Footer from './Footer'
+import Login from './Login'
+import Signup from './Signup'
+import Cart from './Cart'
+import Orders from './Orders'
+import Profile from './Profile'
+import Logout from './Logout'
+import Errpage from './Errpage'
+import {BrowserRouter} from 'react-router-dom'
 function App()
 {
-	return <div>App</div>
+	return <BrowserRouter>
+		
+	</BrowserRouter>
 }
 export default App
